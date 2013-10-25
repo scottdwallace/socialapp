df7217e300eb6e0de621f517f0ccfd073107d4ec
Shashank Golla <shashank.golla@patlive.com>
2013-10-25 10:53:44 -0400

    Initial page and just initialization for the social feed page

--- socialApp/src/com/example/socialapp/MainActivity.java
+++ socialApp/src/com/example/socialapp/MainActivity.java
@@ -3,6 +3,14 @@ package com.example.socialapp;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.ArrayList;
+
 
 public class MainActivity extends Activity {
 
@@ -10,6 +18,18 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+        //adds the respective elements to the list
+        initList();
+        ListView lv = (ListView) findViewById(R.id.listView);
+        SimpleAdapter simpleAdpt;
+        simpleAdpt = new SimpleAdapter(this,socialList,android.R.layout.simple_list_item_1,new String[]
+                {"social"},new int[] {android.R.id.text1});
+
+        lv.setAdapter(simpleAdpt);
+
     }
 
 
@@ -19,5 +39,21 @@ public class MainActivity extends Activity {
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
-    
+
+    List<Map<String,String>> socialList=new ArrayList<Map<String,String>>();
+
+    private void initList()
+    {
+        socialList.add(createSocial("social","Facebook"));
+        socialList.add(createSocial("social","Twitter"));
+        socialList.add(createSocial("social","LinkdIn"));
+
+
+    }
+    private HashMap<String, String> createSocial(String key, String name)
+    {
+        HashMap<String, String> social= new HashMap<String,String>();
+        social.put(key,name);
+        return social;
+    }
 }
